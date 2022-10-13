409d75d1e93944414e613b97d92ee3b2a87a8251
Luis Malvarez Gonzalez <113586607+luismalvarez29@users.noreply.github.com>
2022-10-13 10:10:58 +0200

    Add files via upload

---
+++ Depuración Visual Studio/Código cs/comparacionParoImpar.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace PAR_O_IMPAR
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Dime un número");
+            int n;
+            string b;
+            b = Console.ReadLine();
+            n = int.Parse(b);
+            if (n % 2 == 0)
+            {
+                Console.WriteLine("Número par");
+            }
+
+        }
+    }
+}
